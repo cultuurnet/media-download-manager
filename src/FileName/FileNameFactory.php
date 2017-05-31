df7bc8771d0f4ed99f61db0fd08e090d11246339
Jonas Verhaeghe <jonas@localhost.localdomain>
2017-05-31 12:07:48 +0200

    HER-85 update composer file

---
+++ src/FileName/FileNameFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CultuurNet\MediaDownloadManager\File;
+
+use ValueObjects\Identity\UUID;
+use ValueObjects\StringLiteral\StringLiteral;
+use ValueObjects\Web\Url;
+
+class FileNameFactory implements FileNameFactoryInterface
+{
+
+}
---
+++ src/FileName/FileNameFactoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace CultuurNet\MediaDownloadManager\File;
+
+interface FileNameFactoryInterface
+{
+
+}
